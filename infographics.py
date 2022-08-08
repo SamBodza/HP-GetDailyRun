b98e033d43a11aed7eb908efe753e54d49b60c8e
Bonsa11 <sambodza@hotmail.co.uk>
2022-08-08 10:37:34 +0100

    init commit

---
+++ infographics.py
@@ -0,0 +1,13 @@
+
+error_query = """
+    with errs as (
+        SELECT error_message, COUNT(error_message) as freq
+            FROM librarian.parse_metadata
+        GROUP BY error_message
+    )
+    
+    SELECT *
+        FROM errs
+    WHERE freq > 0
+        ORDER BY freq DESC
+"""
